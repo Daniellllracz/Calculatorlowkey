a20e0eee352ae47bf6788926cfc6e2949f9d52fa
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2024-03-22 11:21:11 +0100

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator;
+public partial class MainWindow : Window
+{
+
+    private double num1 = 0;
+    private double num2 = 0;
+    private string operation = "";
+
+    public MainWindow()
+    {
+        
+    }
+
+}
+
